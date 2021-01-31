e52cefae93d53dfe509222767c69d3a17ce01d20
Jose Aguilar <manugola24@gmail.com>
2021-01-30 21:16:11 -0300

    create resources file

---
+++ src/resources/strings.js
@@ -0,0 +1,23 @@
+export const strings = {
+  home: {
+    welcome: 'Welcome to the Trivia Challenge!',
+    description: 'You will be presented with 10 True or False questions.',
+    score: 'Can you score 100%?',
+    difficulty: 'Please select a difficulty level:',
+    begin: 'Begin'
+  },
+  questions: {
+    question: 'Question',
+    of: 'of',
+    difficulty: '- Difficulty:',
+    category: 'Category:',
+    true: 'True',
+    false: 'False'
+  },
+  results: {
+    difficulty: 'Difficulty:',
+    results: 'Results',
+    score: 'You score',
+    again: 'Play Again?'
+  }
+}
